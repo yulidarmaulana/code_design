f00969f352325a5624fd0148fe2c87332d5751d2
yulidarmaulana <31232250+yulidarmaulana@users.noreply.github.com>
2023-11-25 10:11:31 +0700

    fix : service section

--- bubblebash/src/Service.jsx
+++ bubblebash/src/Service.jsx
@@ -9,14 +9,14 @@ export default function Service() {
           </h2>
 
           <div
-            className="mt-12 grid grid-cols-2 gap-6 md:grid-cols-3"
+            className="mt-12 grid grid-cols-2 gap-6 sm:grid-cols-2 md:grid-cols-3"
             id="frameworks-integration"
           >
             <a
-              className="grid min-w-[7rem] max-w-[18rem] transform cursor-pointer place-items-center rounded-xl border border-blue-gray-50 bg-white px-3 py-2 transition-all hover:scale-105 hover:border-blue-gray-100 hover:bg-blue-gray-50 hover:bg-opacity-25"
+              class="grid mx-auto min-w-[7rem] max-w-[20rem] transform cursor-pointer place-items-center rounded-xl border border-blue-gray-50 bg-white px-3 py-2 transition-all hover:scale-105 hover:border-blue-gray-100 hover:bg-blue-gray-50 hover:bg-opacity-25"
               href="#"
             >
-              <span className="my-6 grid h-24 w-24">
+              <span className="my-6 grid h-24 w-24 place-items-center">
                 <svg
                   className="mx-auto"
                   viewBox="0 0 40 40"
@@ -30,17 +30,95 @@ export default function Service() {
                 </svg>
               </span>
               <h1 className="text-lg font-semibold text-gray-900">
-                UI/UX Designer
+                Web Development
               </h1>
 
-              <p className="text-center font-normal text-gray-600 mb-4">
-                We are ready to make your website more friendly and efficient in
-                the eyes of users
+              <p className="text-center font-normal text-gray-600 mb-4 p-4">
+                Want to create a website that has many features? We have the
+                answer for you.
               </p>
             </a>
 
             <a
-              class="grid min-w-[7rem] max-w-[18rem] transform cursor-pointer place-items-center rounded-xl border border-blue-gray-50 bg-white px-3 py-2 transition-all hover:scale-105 hover:border-blue-gray-100 hover:bg-blue-gray-50 hover:bg-opacity-25"
+              class="grid mx-auto min-w-[7rem] max-w-[20rem] transform cursor-pointer place-items-center rounded-xl border border-blue-gray-50 bg-white px-3 py-2 transition-all hover:scale-105 hover:border-blue-gray-100 hover:bg-blue-gray-50 hover:bg-opacity-25"
+              href="#"
+            >
+              <span className="my-6 grid h-24 w-24 place-items-center">
+                <svg
+                  className="mx-auto"
+                  viewBox="0 0 40 40"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M10.788 14.842h6.865v.557h-6.235v4.189h5.864v.557h-5.864v4.599h6.307v.557h-6.937V14.842zm7.48 0h.73l3.232 4.6 3.304-4.6L30.028 9l-7.383 10.925L26.45 25.3h-.758l-3.462-4.893-3.475 4.893h-.744l3.833-5.376-3.575-5.083zm8.454.557v-.557h7.823v.557h-3.604V25.3h-.63v-9.902h-3.59zm-24.523-.557h.787L13.833 31.4l-4.482-6.1-6.494-9.667-.029 9.668H2.2V14.842zm32.283 9.734a.225.225 0 01-.225-.233.225.225 0 11.45 0 .224.224 0 01-.225.233zm.618-.613h.337c.005.186.138.311.334.311.219 0 .343-.134.343-.386v-1.594h.342v1.596c0 .453-.257.714-.682.714-.4 0-.674-.253-.674-.64zm1.805-.02h.34c.029.214.234.35.53.35.275 0 .477-.145.477-.345 0-.172-.129-.276-.421-.346l-.285-.07c-.4-.096-.582-.293-.582-.624 0-.402.322-.67.804-.67.448 0 .776.268.796.648h-.334c-.032-.208-.21-.338-.467-.338-.27 0-.451.133-.451.336 0 .161.116.254.404.322l.243.061c.453.108.64.296.64.635 0 .432-.328.702-.85.702-.49 0-.82-.258-.844-.661z"
+                    fill="#000"
+                  ></path>
+                </svg>
+              </span>
+              <h1 className="text-lg font-semibold text-gray-900">
+                Web Development
+              </h1>
+
+              <p className="text-center font-normal text-gray-600 mb-4 p-4">
+                Want to create a website that has many features? We have the
+                answer for you.
+              </p>
+            </a>
+
+            <a
+              class="grid mx-auto min-w-[7rem] max-w-[20rem] transform cursor-pointer place-items-center rounded-xl border border-blue-gray-50 bg-white px-3 py-2 transition-all hover:scale-105 hover:border-blue-gray-100 hover:bg-blue-gray-50 hover:bg-opacity-25"
+              href="#"
+            >
+              <span className="my-6 grid h-24 w-24 place-items-center">
+                <svg
+                  className="mx-auto"
+                  viewBox="0 0 40 40"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M10.788 14.842h6.865v.557h-6.235v4.189h5.864v.557h-5.864v4.599h6.307v.557h-6.937V14.842zm7.48 0h.73l3.232 4.6 3.304-4.6L30.028 9l-7.383 10.925L26.45 25.3h-.758l-3.462-4.893-3.475 4.893h-.744l3.833-5.376-3.575-5.083zm8.454.557v-.557h7.823v.557h-3.604V25.3h-.63v-9.902h-3.59zm-24.523-.557h.787L13.833 31.4l-4.482-6.1-6.494-9.667-.029 9.668H2.2V14.842zm32.283 9.734a.225.225 0 01-.225-.233.225.225 0 11.45 0 .224.224 0 01-.225.233zm.618-.613h.337c.005.186.138.311.334.311.219 0 .343-.134.343-.386v-1.594h.342v1.596c0 .453-.257.714-.682.714-.4 0-.674-.253-.674-.64zm1.805-.02h.34c.029.214.234.35.53.35.275 0 .477-.145.477-.345 0-.172-.129-.276-.421-.346l-.285-.07c-.4-.096-.582-.293-.582-.624 0-.402.322-.67.804-.67.448 0 .776.268.796.648h-.334c-.032-.208-.21-.338-.467-.338-.27 0-.451.133-.451.336 0 .161.116.254.404.322l.243.061c.453.108.64.296.64.635 0 .432-.328.702-.85.702-.49 0-.82-.258-.844-.661z"
+                    fill="#000"
+                  ></path>
+                </svg>
+              </span>
+              <h1 className="text-lg font-semibold text-gray-900">
+                Content Writer
+              </h1>
+
+              <p className="text-center font-normal text-gray-600 mb-4 p-4">
+                We provide interesting content and can attract customers for you
+              </p>
+            </a>
+
+            <a
+              class="grid mx-auto min-w-[7rem] max-w-[20rem] transform cursor-pointer place-items-center rounded-xl border border-blue-gray-50 bg-white px-3 py-2 transition-all hover:scale-105 hover:border-blue-gray-100 hover:bg-blue-gray-50 hover:bg-opacity-25"
+              href="#"
+            >
+              <span className="my-6 grid h-24 w-24 place-items-center">
+                <svg
+                  className="mx-auto"
+                  viewBox="0 0 40 40"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M10.788 14.842h6.865v.557h-6.235v4.189h5.864v.557h-5.864v4.599h6.307v.557h-6.937V14.842zm7.48 0h.73l3.232 4.6 3.304-4.6L30.028 9l-7.383 10.925L26.45 25.3h-.758l-3.462-4.893-3.475 4.893h-.744l3.833-5.376-3.575-5.083zm8.454.557v-.557h7.823v.557h-3.604V25.3h-.63v-9.902h-3.59zm-24.523-.557h.787L13.833 31.4l-4.482-6.1-6.494-9.667-.029 9.668H2.2V14.842zm32.283 9.734a.225.225 0 01-.225-.233.225.225 0 11.45 0 .224.224 0 01-.225.233zm.618-.613h.337c.005.186.138.311.334.311.219 0 .343-.134.343-.386v-1.594h.342v1.596c0 .453-.257.714-.682.714-.4 0-.674-.253-.674-.64zm1.805-.02h.34c.029.214.234.35.53.35.275 0 .477-.145.477-.345 0-.172-.129-.276-.421-.346l-.285-.07c-.4-.096-.582-.293-.582-.624 0-.402.322-.67.804-.67.448 0 .776.268.796.648h-.334c-.032-.208-.21-.338-.467-.338-.27 0-.451.133-.451.336 0 .161.116.254.404.322l.243.061c.453.108.64.296.64.635 0 .432-.328.702-.85.702-.49 0-.82-.258-.844-.661z"
+                    fill="#000"
+                  ></path>
+                </svg>
+              </span>
+              <h1 className="text-lg font-semibold text-gray-900">Branding</h1>
+
+              <p className="text-center font-normal text-gray-600 mb-4 p-2">
+                Create visual branding with amazing result, you just sit back
+                and relax
+              </p>
+            </a>
+
+            <a
+              class="grid mx-auto min-w-[7rem] max-w-[20rem] transform cursor-pointer place-items-center rounded-xl border border-blue-gray-50 bg-white px-3 py-2 transition-all hover:scale-105 hover:border-blue-gray-100 hover:bg-blue-gray-50 hover:bg-opacity-25"
               href="#"
             >
               <span class="my-6 grid h-24 w-24 place-items-center">
@@ -57,17 +135,17 @@ export default function Service() {
                 </svg>
               </span>
               <h1 className="text-lg font-semibold text-gray-900">
-                UI/UX Designer
+                Editing Video
               </h1>
 
-              <p className="text-center font-normal text-gray-600 mb-4">
-                We are ready to make your website more friendly and efficient in
-                the eyes of users
+              <p className="text-center font-normal text-gray-600 mb-4 p-2">
+                Want to create a website that has many features? We have the
+                answer for you.
               </p>
             </a>
 
             <a
-              class="grid min-w-[7rem] max-w-[18rem] transform cursor-pointer place-items-center rounded-xl border border-blue-gray-50 bg-white px-3 py-2 transition-all hover:scale-105 hover:border-blue-gray-100 hover:bg-blue-gray-50 hover:bg-opacity-25"
+              class="grid mx-auto min-w-[7rem] max-w-[20rem] transform cursor-pointer place-items-center rounded-xl border border-blue-gray-50 bg-white px-3 py-2 transition-all hover:scale-105 hover:border-blue-gray-100 hover:bg-blue-gray-50 hover:bg-opacity-25"
               href="#"
             >
               <span class="my-6 grid h-24 w-24 place-items-center">
@@ -84,144 +162,12 @@ export default function Service() {
                 </svg>
               </span>
               <h1 className="text-lg font-semibold text-gray-900">
-                UI/UX Designer
+                IIlustration
               </h1>
 
-              <p className="text-center font-normal text-gray-600 mb-4">
-                We are ready to make your website more friendly and efficient in
-                the eyes of users
+              <p className="text-center font-normal text-gray-600 mb-4 p-2">
+                Create beauty illustrasion as you need, and make it interesting
               </p>
-            </a>
-
-            <a
-              class="grid w-full min-w-[7rem] transform cursor-pointer place-items-center rounded-xl border border-blue-gray-50 bg-white px-3 py-2 transition-all hover:scale-105 hover:border-blue-gray-100 hover:bg-blue-gray-50 hover:bg-opacity-25"
-              href="#"
-            >
-              <span class="my-6 grid h-24 w-24 place-items-center">
-                <svg
-                  class="mx-auto"
-                  viewBox="0 0 40 40"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M31.881 11.61L20.886 31.563a.593.593 0 01-1.04.004L8.633 11.611c-.251-.446.125-.987.624-.897l11.007 1.997a.59.59 0 00.212 0l10.777-1.994c.497-.092.875.445.628.893z"
-                    fill="url(#vite_svg__paint0_linear_41_6732)"
-                  ></path>
-                  <path
-                    d="M25.506 8.096l-8.137 1.618a.302.302 0 00-.241.28l-.5 8.578a.3.3 0 00.365.314l2.265-.531c.212-.05.404.14.36.356l-.673 3.345a.3.3 0 00.38.35l1.399-.43a.3.3 0 01.38.35l-1.07 5.255c-.067.328.364.507.543.226l.12-.189 6.63-13.428c.111-.225-.08-.481-.323-.433l-2.332.456a.301.301 0 01-.344-.381l1.522-5.355a.301.301 0 00-.345-.381z"
-                    fill="url(#vite_svg__paint1_linear_41_6732)"
-                  ></path>
-                  <defs>
-                    <linearGradient
-                      id="vite_svg__paint0_linear_41_6732"
-                      x1="8.359"
-                      y1="10.001"
-                      x2="22.306"
-                      y2="28.665"
-                      gradientUnits="userSpaceOnUse"
-                    >
-                      <stop stop-color="#41D1FF"></stop>
-                      <stop offset="1" stop-color="#BD34FE"></stop>
-                    </linearGradient>
-                    <linearGradient
-                      id="vite_svg__paint1_linear_41_6732"
-                      x1="19.631"
-                      y1="8.535"
-                      x2="22.178"
-                      y2="25.757"
-                      gradientUnits="userSpaceOnUse"
-                    >
-                      <stop stop-color="#FFEA83"></stop>
-                      <stop offset="0.083" stop-color="#FFDD35"></stop>
-                      <stop offset="1" stop-color="#FFA800"></stop>
-                    </linearGradient>
-                  </defs>
-                </svg>
-              </span>
-            </a>
-            <a
-              class="grid w-full min-w-[7rem] transform cursor-pointer place-items-center rounded-xl border border-blue-gray-50 bg-white px-3 py-2 transition-all hover:scale-105 hover:border-blue-gray-100 hover:bg-blue-gray-50 hover:bg-opacity-25"
-              href="#"
-            >
-              <span class="my-6 grid h-24 w-24 place-items-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="72"
-                  height="72"
-                  viewBox="0 0 1280 1280"
-                  fill="none"
-                  class="mx-auto"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
-                    d="M815.039 94.6439C824.758 106.709 829.714 122.99 839.626 155.553L1056.17 866.901C976.107 825.368 889.072 795.413 797.281 779.252L656.29 302.798C653.983 295.002 646.822 289.654 638.693 289.654C630.542 289.654 623.368 295.03 621.08 302.853L481.795 779.011C389.579 795.1 302.146 825.109 221.741 866.793L439.347 155.388L439.348 155.388C449.291 122.882 454.262 106.629 463.982 94.5853C472.562 83.9531 483.723 75.6958 496.4 70.6002C510.76 64.8284 527.756 64.8284 561.749 64.8284H717.174C751.212 64.8284 768.23 64.8284 782.603 70.6123C795.292 75.7184 806.459 83.9923 815.039 94.6439Z"
-                    fill="url(#paint0_linear_709_110)"
-                  ></path>
-                  <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
-                    d="M840.951 900.754C805.253 931.279 734.002 952.097 651.929 952.097C551.197 952.097 466.767 920.737 444.363 878.561C436.354 902.732 434.558 930.396 434.558 948.068C434.558 948.068 429.281 1034.84 489.636 1095.2C489.636 1063.86 515.042 1038.46 546.381 1038.46C600.097 1038.46 600.036 1085.32 599.987 1123.34C599.986 1124.48 599.984 1125.61 599.984 1126.73C599.984 1184.44 635.255 1233.91 685.416 1254.77C677.924 1239.36 673.721 1222.05 673.721 1203.77C673.721 1148.73 706.034 1128.23 743.588 1104.41L743.588 1104.41C773.469 1085.46 806.668 1064.41 829.548 1022.17C841.486 1000.13 848.265 974.893 848.265 948.068C848.265 931.573 845.702 915.676 840.951 900.754Z"
-                    fill="#FF5D01"
-                  ></path>
-                  <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
-                    d="M840.951 900.754C805.253 931.279 734.002 952.097 651.929 952.097C551.197 952.097 466.767 920.737 444.363 878.561C436.354 902.732 434.558 930.396 434.558 948.068C434.558 948.068 429.281 1034.84 489.636 1095.2C489.636 1063.86 515.042 1038.46 546.381 1038.46C600.097 1038.46 600.036 1085.32 599.987 1123.34C599.986 1124.48 599.984 1125.61 599.984 1126.73C599.984 1184.44 635.255 1233.91 685.416 1254.77C677.924 1239.36 673.721 1222.05 673.721 1203.77C673.721 1148.73 706.034 1128.23 743.588 1104.41L743.588 1104.41C773.469 1085.46 806.668 1064.41 829.548 1022.17C841.486 1000.13 848.265 974.893 848.265 948.068C848.265 931.573 845.702 915.676 840.951 900.754Z"
-                    fill="url(#paint1_linear_709_110)"
-                  ></path>
-                  <defs>
-                    <linearGradient
-                      id="paint0_linear_709_110"
-                      x1="882.997"
-                      y1="27.1132"
-                      x2="638.955"
-                      y2="866.902"
-                      gradientUnits="userSpaceOnUse"
-                    >
-                      <stop stop-color="#000014"></stop>
-                      <stop offset="1" stop-color="#150426"></stop>
-                    </linearGradient>
-                    <linearGradient
-                      id="paint1_linear_709_110"
-                      x1="1001.68"
-                      y1="652.45"
-                      x2="790.326"
-                      y2="1094.91"
-                      gradientUnits="userSpaceOnUse"
-                    >
-                      <stop stop-color="#FF1639"></stop>
-                      <stop
-                        offset="1"
-                        stop-color="#FF1639"
-                        stop-opacity="0"
-                      ></stop>
-                    </linearGradient>
-                  </defs>
-                </svg>
-              </span>
-            </a>
-            <a
-              class="grid w-full min-w-[7rem] transform cursor-pointer place-items-center rounded-xl border border-blue-gray-50 bg-white px-3 py-2 transition-all hover:scale-105 hover:border-blue-gray-100 hover:bg-blue-gray-50 hover:bg-opacity-25"
-              href="#"
-            >
-              <span class="my-6 grid h-24 w-24 place-items-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="64"
-                  height="64"
-                  viewBox="0 0 28 28"
-                  focusable="false"
-                >
-                  <title>Gatsby</title>
-                  <circle cx="14" cy="14" r="14" fill="#639"></circle>
-                  <path
-                    fill="#fff"
-                    d="M6.2 21.8C4.1 19.7 3 16.9 3 14.2L13.9 25c-2.8-.1-5.6-1.1-7.7-3.2zm10.2 2.9L3.3 11.6C4.4 6.7 8.8 3 14 3c3.7 0 6.9 1.8 8.9 4.5l-1.5 1.3C19.7 6.5 17 5 14 5c-3.9 0-7.2 2.5-8.5 6L17 22.5c2.9-1 5.1-3.5 5.8-6.5H18v-2h7c0 5.2-3.7 9.6-8.6 10.7z"
-                  ></path>
-                </svg>
-              </span>
             </a>
           </div>
         </div>
